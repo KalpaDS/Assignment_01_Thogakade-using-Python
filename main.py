48b6028686ba9d36a16f69c48996a7920df2b8d4
KalpaDS <desilvakalpa5@gmail.com>
2021-11-02 10:38:30 +0530

    '#updatedmainfiles'

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+import item
+import user
+
+if __name__ == "__main__":
+    arguments = sys.argv
+
+    section = arguments[1]
+    command = arguments[2]
+    params = arguments[3:]
+    print(section, command, params)
+
+    if section == "user":
+        user.set_command_and_params(command, params)
+    elif section == "item":
+        item.set_command_and_params(command, params)
